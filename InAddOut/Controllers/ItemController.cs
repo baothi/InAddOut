502051491474dcf12800192d7ea9549b7c8a47d3
baothi <baothi246@gmail.com>
2021-03-26 17:32:10 +0700

    add create

---
+++ InAddOut/Controllers/ItemController.cs
@@ -0,0 +1,31 @@
+﻿using InAddOut.Data;
+using InAddOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAddOut.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Item> obList = _db.Items;
+            return View(obList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
--- InAddOut/Models/Item.cs
+++ InAddOut/Models/Item.cs
@@ -12,5 +12,7 @@ namespace InAddOut.Models
         public int Id { get; set; }
 
         public string Borrower { get; set; }
+        public string Lender { get; set; }
+        public string ItemName { get; set; }
     }
 }
