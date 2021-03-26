74760882f00056886fb980f3fcb94528d44249b6
baothi <baothi246@gmail.com>
2021-03-26 10:24:46 +0700

    connect db and create db

---
+++ InAddOut/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAddOut.Models
+{
+    public class Item
+    {
+        [Key]
+        public int id { get; set; }
+        public string Borrower { get; set; }
+    }
+}
