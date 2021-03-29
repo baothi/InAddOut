e385b2f12735a217498f3b6a46a9f23cac90199c
baothi <baothi246@gmail.com>
2021-03-29 15:00:58 +0700

    add type

---
+++ InAddOut/Controllers/ExpenseTypeController.cs
@@ -0,0 +1,105 @@
+﻿using InAddOut.Data;
+using InAddOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAddOut.Controllers
+{
+    public class ExpenseTypeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ExpenseTypeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<ExpenseType> obList = _db.ExpenseTypes;
+            return View(obList);
+        }
+
+        // GET-CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        // POST-CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ExpenseType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ExpenseTypes.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //Get Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.ExpenseTypes.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // POST-Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.ExpenseTypes.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.ExpenseTypes.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET-Update
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.ExpenseTypes.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        // POST-Update
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(ExpenseType obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ExpenseTypes.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    }
+}
